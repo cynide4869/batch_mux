7275914e1ed3c1489515c7c4332f6afb1c0e1bf7
CyNide4869 <dichromang@gmail.com>
2021-12-24 00:07:20 +0530

    Add ignore existing function

--- Batch_Mux.py
+++ Batch_Mux.py
@@ -4,7 +4,7 @@ import pymkv
 
 parent_directory = (Path.cwd()).resolve()
 mkvfiles_directory = (parent_directory / 'mkvfiles').resolve()
-attachments_directory = (parent_directory / 'attachments').resolve()
+attachments_directory = (parent_directory / 'append').resolve()
 output_directory = (parent_directory / 'output').resolve()
 
 def create_folder():
@@ -21,7 +21,7 @@ def get_list(source_directory):
 	return sorted(temp_list)
 
 
-def append(attachments_folder, mkv):
+def append(attachments_folder, mkv, insert):
 	'''Appends the fonts, subtitle tracks and chapters if present'''
 
 	font_folders = []
@@ -29,44 +29,65 @@ def append(attachments_folder, mkv):
 	chapter = None
 
 	for item in attachments_folder.iterdir():
-		if item.is_dir():
+		if item.is_dir() and '2' not in insert:
 			font_folders.append(item)
-		elif item.suffix == '.ass':
+		elif item.suffix == '.ass' and '3' not in insert:
 			subs.append(item)
-		elif 'chapters' in item.name.lower() and item.suffix == '.xml':
+		elif ('chapters' in item.name.lower() and item.suffix == '.xml') and '1' not in insert:
 			chapter = item
 		else:
 			continue
 
 	if font_folders:
-		print('\nAdding fonts:\n')
+		print('\nAdding fonts:')
 		for font_folder in font_folders:
 			for font in font_folder.iterdir():
-				print('{}'.format(font.name))
+				print('--{}'.format(font.name))
 				mkv.add_attachment(str(font))
 	else:
-		print('\nNo fonts present')
+		print('\nNo fonts to append')
 
 	if subs:
-		print('\nAdding subtitles:\n')
+		print('\nAdding subtitles:')
 		for subtitle in subs:
-			print('{}'.format(subtitle.name))
+			print('--{}'.format(subtitle.name))
 			mkv.add_track(str(subtitle))
 	else:
-		print('\nNo subtitles present')
+		print('\nNo subtitles to append')
 
 	if chapter:
-		print('\nAdding chapter: ', chapter.name)
+		print('\nAdding chapter:\n--{}'.format(chapter.name))
 		mkv.chapters(str(chapter))
 	else:
-		print('\nNo chapters present')
+		print('\nNo chapters to append')
 
 
 def muxing(mkv, mkvfile):
 	'''Muxes the mkv file with the changes made'''
 
 	mkv.mux(output_directory / mkvfile.name, silent=True)
-	print('Done\n----------------------------------')
+	print('\nDone.')
+
+
+def ignore_existing(mkv, choice):
+	'''Ignore certain MKV file properties which are present in the file'''
+
+	if '0' in choice:
+		return
+
+	print('\nIgnoring the following properties present in the file')
+	if '1' in choice:
+		print('--chapters')
+		mkv.no_chapters()
+	if '2' in choice:
+		print('--attachments')
+		mkv.no_attachments()
+	if '3' in choice:
+		print('--global tags')
+		mkv.no_global_tags()
+	if '4' in choice:
+		print('--track tags')		
+		mkv.no_track_tags()
 
 
 def main():
@@ -74,10 +95,20 @@ def main():
 	mkvfiles = get_list(mkvfiles_directory)
 	attachments = get_list(attachments_directory)
 
+	print('\nChoose the properties to ignore which are already present in the file (Remove properties in the final output)')
+	print('--1) Chapters\n--2) Attachments (Fonts)\n--3) Global Tags\n--4) Track Tags')
+	existing = input('Enter the numbers (Enter 0 to skip this menu): ')
+
+	print('\nChoose the properties to ignore which are to be appended to the file (Won\'t append to the final output)')
+	print('--1) Chapters\n--2) Attachments (Fonts)\n--3) Subtitles')
+	insert = input('Enter the numbers (Enter 0 to skip this menu, adds all the files present in the attachments folder): ')
+
 	for i, mkvfile in enumerate(mkvfiles):
-		print('Working File: ', mkvfile.name)
+		print('---------------------------------------------------------------------------')
+		print('\nWorking File: ', mkvfile.name)
 		mkv = pymkv.MKVFile(mkvfile)
-		append(attachments[i], mkv)
+		ignore_existing(mkv, existing)
+		append(attachments[i], mkv, insert)
 		muxing(mkv, mkvfile)
 
 
--- README.md
+++ README.md
@@ -1,25 +1,70 @@
+# Batch Mux
+
+This script is used to batch append Chapters, fonts and subtitles into the mkvfiles.\
+Follows the animetosho attachments download folder structure.\
+(Created for anime use)
+
 ```
 -------Folder Structure-------
 
 root
-|--attachments
-|  |--attachmentsfolder1
-|  |  |--fonts
+|
+|--append
+|  |--attachments_folder_1
+|  |  |--attachments
 |  |  |  |--font1.ttf
 |  |  |  `--font2.ttf
+|  |  |
 |  |  |--subtitle1.ass
-|  |  `--subtitle2.ass
-|  |--attachmentsfolder2
-|  |  |--fonts
+|  |  |--subtitle2.ass
+|  |  |
+|  |  `--chapters.xml
+|  |
+|  |--attachments_folder_2
+|  |  |--attachments
 |  |  |  |--font1.ttf
 |  |  |  `--font2.ttf
+|  |  |
 |  |  |--subtitle1.ass
-|  `  `--subtitle2.ass
+|  |  |--subtitle2.ass
+|  |  |
+|  `  `--chapters.xml
+|
 |--mkvfiles
 |  |--mkv1.mkv
 |  `--mkv2.mkv
-|--output
+|
+|--output (will be created)
 |  |--mkvout1.mkv
 |  `--mkvout2.mkv
-`--BatchMux.py
+|
+`--Batch_Mux.py
 ```
+
+The above folder structure is to be followed.
+
+# Dependencies
+
+Download the requirements.txt file and run
+```
+pip install -r requirements.txt
+```
+or just run
+```
+pip install pymkv
+```
+
+> Make sure mkvmerge has been added to PATH or copy mkvmerge.exe to the root (or parent) folder for the script to detect mkvmerge
+
+# Run
+
+To run the script, open the terminal in the root folder and run
+```
+python Batch_Mux.py
+```
+
+# Links
+
+[pymkv docs](https://pymkv.shel.dev/en/stable/)\
+[mkvtoolnix](https://mkvtoolnix.download/downloads.html)\
+[python](https://www.python.org/downloads/)
